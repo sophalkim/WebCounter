5b43dfff28b372c7aef2203622b40f6501cde699
sophalkim <sophal.kim.work@gmail.com>
2014-10-03 20:38:09 -0700

    Cannot find resources error

--- src/servlet/FileCounter.java
+++ src/servlet/FileCounter.java
@@ -1,12 +1,14 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.FileDao;
 
@@ -24,21 +26,41 @@ public class FileCounter extends HttpServlet {
      */
     public FileCounter() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(true);
+		session.setMaxInactiveInterval(5);
+		response.setContentType("text/plain");
+		PrintWriter out = response.getWriter();
+		if (session.isNew()) {
+			count++;
+		}
+		out.println("This site has been accessed" + count + "times.");
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
+	@Override
+	public void init() throws ServletException {
+		dao = new FileDao();
+		try {
+			count = dao.getCount();
+		} catch (Exception e) {
+			getServletContext().log("An exception occurred in FileCounter", e);
+			throw new ServletException("An exception occurred in FileCounter" + e.getMessage());
+		}
+	}
+	
+	public void destroy() {
+		super.destroy();
+		try {
+			dao.save(count);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
